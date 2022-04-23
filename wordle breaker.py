92e57f299e9e5bca3ade234d8b5b725cd9bc9f17
JackyHe398 <62739796+JackyHe398@users.noreply.github.com>
2022-04-23 18:00:53 +0800

    Update wordle breaker.py
    optimise the output part and add some discription

--- wordle breaker.py
+++ wordle breaker.py
@@ -1,26 +1,23 @@
 ban_list = []
-ban_list_pre = []
-correct_word_input = "     "
-correct_word = ["","","","",""]
-no_pos_input = ""
+correct_word = [""for _ in range(5)]
 no_pos = [[]for _ in range(5)]
 all_no_pos = []
-ToF = "null" # y/n/""
 file = open("wb.txt")
 ordered_letter = []
 con = False
 keep_list = []
 
 
-def separate_correct_letter_to_list(correct_word_input):
+def separate_correct_letter_to_list(inp):
     for _ in range(5):
-        if correct_word_input[_:_+1] != " ":
-            correct_word[_] = correct_word_input[_:_+1]
+        if inp[_:_+1] != " ":
+            correct_word[_] = inp[_:_+1]
     
     print(correct_word)
 
 
 def input_banned_list():
+    print(f"\nbanned word now is: {ban_list}")
     print(f"Input the baned word: ", end="")
     ban_list_pre = sorted(str.lower(input()))
     for i in range(len(ban_list_pre)):
@@ -29,19 +26,20 @@ def input_banned_list():
     
     ban_list.sort()
 
-def input_correct_letter():
-    ToF = "null"
-    while not (ToF == "y" or ToF == "n" or ToF == ""):
-        print(f"The word now is {correct_word}, is it correct?(y/n): ",end = "")
-        ToF = str.lower(input())
 
-    if ToF == "y" or ToF == "":
-        ToF = "null"
+def input_correct_letter():
+    tof = "null"
+    print()
+    while not (tof == "y" or tof == "n" or tof == ""):
+        print(f"The word now is {correct_word}\nIs it correct?(y/n): ", end="")
+        tof = str.lower(input())
+
+    if tof == "y" or tof == "":
         return
 
-    ToF = "n" #initialise ToF
+    tof = "n"
     
-    while ToF == "n":
+    while tof == "n":
         print("Input the World You know(replace the letter with space if you don't know): ", end="")
         correct_word_input = input()
         if len(correct_word_input) != 5:
@@ -49,15 +47,15 @@ def input_correct_letter():
         if not(all(x.isalpha() or x.isspace() for x in correct_word_input)):
             continue
 
-        print(f"The word now is {correct_word_input}, is it correct?(y/n)")
-        ToF = str.lower(input())
+        print(f"The word now is \"{correct_word_input}\"\nIs it correct?(y/n)", end="")
+        tof = str.lower(input())
         separate_correct_letter_to_list(correct_word_input)
 
-    ToF = "null"
 
 def input_no_pos_letter():
+    print()
     for _ in range(5):
-        print(f"Input the wrong position letter in {_+1} position(yellow one): ",end='')
+        print(f"Input the wrong position letter in {_+1} position(yellow one): ", end='')
         no_pos_input = list(input())
         for j in range(len(no_pos_input)):
             if no_pos_input[j].isalpha():
@@ -66,11 +64,13 @@ def input_no_pos_letter():
                 if no_pos_input[j] not in all_no_pos:
                     all_no_pos.append(no_pos_input[j])
 
+
+print("+="*10+"Wordle Breaker"+"+="*10)
 while True:
-    input_banned_list()
+    input_banned_list()  # 3 type of inputting
     input_correct_letter()
     input_no_pos_letter()
-    remove_list = []
+    remove_list = []  # initialise to empty array
 
     for line in file:
         word = line.replace('\n', '')
@@ -80,24 +80,22 @@ while True:
         word = keep_list[_]
         ordered_letter = list(keep_list[_])
         for j in range(5):
-            if ordered_letter[j] in ban_list:
+            if ordered_letter[j] in ban_list:  # these three output is the same
+                con = True  # throw the wrong word out
+                break
+            elif not (ordered_letter[j] == correct_word[j] or correct_word[j] == ""):
                 con = True
                 break
-            
-            if not (ordered_letter[j] == correct_word[j] or correct_word[j] == ""):
-                con = True
-                break
-
-            if ordered_letter[j] in no_pos[j]:
+            elif ordered_letter[j] in no_pos[j]:
                 con = True
                 break
         
-        for j in range(len(all_no_pos)):
+        for j in range(len(all_no_pos)): 
             if all_no_pos[j] not in word:
                 con = True
                 break
 
-        if con:
+        if con:  # break the big "for" and remove the wrong words
             con = False
             remove_list.append(keep_list[_])
             continue
